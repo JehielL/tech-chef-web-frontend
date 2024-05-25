d805df3d01f3919174b4bbece760f6f7f0d5be07
JehielL <jehiel.py@gmail.com>
2024-05-25 22:47:42 +0200

    Nuevos cambios.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import * as AOS from 'aos'; 
+
 
 @Component({
   selector: 'app-home',
@@ -8,7 +10,10 @@ import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  ngOnInit(): void {
+    AOS.init();
+  }
   title = 'tech-chef-web';
   collapsed = true;
   images = [944, 1011, 984].map((n) => `https://foodservicemagazine.es/wp-content/uploads/2017/08/restaurantes-tecnologicos.jpg`);

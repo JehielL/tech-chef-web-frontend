79a16bbe5e4d90c830eba078268e31208659d0d2
JehielL <jehiel.py@gmail.com>
2025-08-04 01:13:24 +0200

    Mejorado diseño blog TODO: Crear contenido acorde.

--- src/app/blog-tech/blog-tech.component.ts
+++ src/app/blog-tech/blog-tech.component.ts
@@ -1,19 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-blog-tech',
   standalone: true,
   imports: [],
   templateUrl: './blog-tech.component.html',
-  styleUrl: './blog-tech.component.css'
+  styleUrl: './blog-tech.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class BlogTechComponent implements OnInit {
 
   activedLoader = true;
+  
   ngOnInit(): void {
+    // Inicializar AOS
+    AOS.init({
+      duration: 800,
+      easing: 'ease-out-cubic',
+      once: true,
+      offset: 100
+    });
+
     setTimeout(() => {
       this.activedLoader = false;
+      // Refresh AOS después de cargar
+      setTimeout(() => {
+        AOS.refresh();
+      }, 100);
     }, 2300); 
+    
     window.scrollTo(0, 0); 
   }
 }
